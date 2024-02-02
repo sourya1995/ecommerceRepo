36371d85632efc27d53e422cf42d7fc27286a430
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-02 10:54:20 +0530

    feature: Global Exception Handler

---
+++ ecommerce/src/main/java/com/sourya/ecommerce/exceptions/ErrorUtils.java
@@ -0,0 +1,15 @@
+package com.sourya.ecommerce.exceptions;
+
+public class ErrorUtils {
+    private ErrorUtils() {
+
+    }
+
+    public static Error createError(final String errMsgKey, final String errorCode, final Integer httpStatusCode){
+        Error error = new Error();
+        error.setMessage(errMsgKey);
+        error.setErrorCode(errorCode);
+        error.setStatus(httpStatusCode);
+        return error;
+    }
+}
