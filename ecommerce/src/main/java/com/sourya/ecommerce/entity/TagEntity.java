286aadf6553360e73c639e9a53d701ce4ffc8334
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-04 11:24:13 +0530

    feature: adding database entities

---
+++ ecommerce/src/main/java/com/sourya/ecommerce/entity/TagEntity.java
@@ -0,0 +1,37 @@
+package com.sourya.ecommerce.entity;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tag")
+public class TagEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotNull(message = "Product name is required.")
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TagEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TagEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
